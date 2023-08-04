946f2a7adf9b9824157d0da231a496539fdf6aeb
Caleb Saephan <me@calebsaephan.dev>
2023-08-04 16:34:09 -0700

    add sale banner

--- pages/index.vue
+++ pages/index.vue
@@ -20,6 +20,13 @@ const routes = [
 </script>
 <template>
     <div class="container grid grid-cols-12 gap-2 mx-auto max-w-6xl">
+        <div class="col-span-full">
+            <div
+                class="flex justify-center items-center bg-gradient-to-bl from-yellow-300 to-yellow-200 h-32 mb-4 font-bold text-2xl italic">
+                30% Off Summer
+                Sale
+            </div>
+        </div>
         <div class="col-span-3">
             <ul>
                 <a v-for="route in routes" :href="route.to" class="hover:text-sky-500 font-semibold">
